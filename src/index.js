835d533eb0348d236fecc3c0669600787c5def94
Dmitry Uvarov <diman_niks@mail.ru>
2021-01-21 16:53:32 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+let arrUnits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let arrTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
 module.exports = function toReadable (number) {
-  
+    let result = '';
+    let strNumber = String(number);
+
+    let objNumber = {
+        hundreds: strNumber.charAt(strNumber.length - 3),
+        tens: strNumber.charAt(strNumber.length - 2),
+        units: strNumber.charAt(strNumber.length - 1)
+    };
+
+    if (objNumber.hundreds) {
+        result += `${arrUnits[objNumber.hundreds]} hundred`;
+    }
+
+    if (objNumber.tens == 1 || objNumber.tens != 0) {
+        if (objNumber.hundreds) result += ` `;
+
+        if (objNumber.tens == 1) result += `${arrTens[`${objNumber.tens}${objNumber.units}`]}`;
+        if (objNumber.tens != 0 && objNumber.tens != 1) result += `${arrTens[objNumber.tens]}`;
+    }
+
+    //if (num.units) {
+        if (objNumber.tens != 1 && objNumber.units != 0) {
+            if (objNumber.tens) result += ` `;
+            result += `${arrUnits[objNumber.units]}`;
+        }
+    //}
+
+    return result ? result : 'zero';
 }
